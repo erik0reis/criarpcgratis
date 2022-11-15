6d435235b48afd7086b68d1ccb1b5f1e3cef095e
erik0reis <58051404+erik0reis@users.noreply.github.com>
2022-11-15 16:47:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function choosetime(hour, minute) {
 
     var enddate = new Date(date.toString());
 
-    enddate.setUTCMinutes(enddate.getUTCMinutes() + 30);
+    enddate.setUTCMinutes(enddate.getUTCMinutes() + 45);
 
     document.getElementById('chooseddateandtime').innerHTML = `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()} às ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')} até ${enddate.getHours().toString().padStart(2, '0')}:${enddate.getMinutes().toString().padStart(2, '0')}`;
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
